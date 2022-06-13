e221d24e578d7ed85cdbd36fa4aa3545c6abc6ed
Doriane77 <dlolliadu77@hotmail.fr>
2022-06-13 17:55:12 +0200

    Base of Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Contact from "./Content/Contact/Contact";
+import Creation from "./Content/Creation/Creation";
+import Deliveroo from "./Content/SitePage/Deliveroo";
+import Home from "./Content/Home/Home";
+import Marmiton from "./Content/SitePage/Marmiton";
+import Netflix from "./Content/SitePage/Netflix";
+import Tripadvisor from "./Content/SitePage/Tripadvisor";
+import Vinted from "./Content/SitePage/Vinted";
+
+import Header from "./Components/Header/Header";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHouse,
+  faArrowLeft,
+  faLocationDot,
+  faPhone,
+  faCar,
+  faLocationArrow,
+  faMapLocation,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faHouse,
+  faArrowLeft,
+  faLocationDot,
+  faPhone,
+  faCar,
+  faLocationArrow,
+  faMapLocation,
+  faEyeSlash
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/creation" element={<Creation />} />
+          <Route path="/creation/vinted" element={<Vinted />} />
+          <Route path="/creation/deliveroo" element={<Deliveroo />} />
+          <Route path="/creation/tripadvisor" element={<Tripadvisor />} />
+          <Route path="/creation/netflix" element={<Netflix />} />
+          <Route path="/creation/marmiton" element={<Marmiton />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
